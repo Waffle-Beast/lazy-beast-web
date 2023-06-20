aa0f8479fb57689cf9e936d21ee01e42e21cd356
Lazy <90242387+Lazy-Beast@users.noreply.github.com>
2023-06-20 17:04:04 +0300

    Add files via upload

---
+++ JavaScript/menu_for_small_screens.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector('.adaptive-menu');
+const nav = document.querySelector('.navigation');
+const sections = document.querySelectorAll('section')
+const body = document.querySelector('body')
+
+console.log(body)
+
+menu.addEventListener('click', () => {
+  nav.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+
+sections.forEach(section => {
+  section.onclick = function(){
+    nav.classList.remove('active');
+    menu.classList.remove('active');
+  };
+});
+
